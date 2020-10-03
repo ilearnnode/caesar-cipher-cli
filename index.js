c7cde956754e7b480e354239f469e2b691ec2c47
ilearnnode <youhohoho@gmail.com>
2020-10-04 01:10:28 +0300

    feat: parse cli parameters

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const argv = require('minimist')(process.argv.slice(2));
+
+// -s, --shift: a shift
+// -i, --input: an input file
+// -o, --output: an output file
+// -a, --action: an action encode/decode
+
+// TODO: add validation
+const shift = argv.s || argv.shift;
+const action = argv.a || argv.action;
+const input = argv.i || argv.input;
+const output =  argv.o || argv.output;
